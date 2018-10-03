3e63feb4299e0e297e2dbbc01a45a110a1a3e91b
npatterson26 <nathanpatterson@unomaha.edu>
2018-10-03 16:26:05 -0500

    initial commit

---
+++ crm/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
+app_name = 'crm'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'),
+         name='password_change'),
+    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         name='password_change'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('register/', views.register, name='register'),
+    re_path(r'^home/$', views.home, name='home'),
+    path('customer_list', views.customer_list, name='customer_list'),
+    path('customer/<int:pk>/edit/', views.customer_edit, name='customer_edit'),
+    path('customer/<int:pk>/delete/', views.customer_delete, name='customer_delete'),
+    path('service_list', views.service_list, name='service_list'),
+    path('service/create/', views.service_new, name='service_new'),
+    path('service/<int:pk>/edit/', views.service_edit, name='service_edit'),
+    path('service/<int:pk>/delete/', views.service_delete, name='service_delete'),
+    path('product_list', views.product_list, name='product_list'),
+    path('product/create/', views.product_new, name='product_new'),
+    path('product/<int:pk>/edit/', views.product_edit, name='product_edit'),
+    path('product/<int:pk>/delete/', views.product_delete, name='product_delete'),
+    path('customer/<int:pk>/summary/', views.summary, name='summary'),
+]
